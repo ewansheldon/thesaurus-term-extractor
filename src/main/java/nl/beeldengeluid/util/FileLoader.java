9e5bbb65ba022328f5a62452433b17590706277e
Ewan Sheldon <ewansheldon@gmail.com>
2025-12-09 16:58:38 +0100

    loads lines from csv

--- src/main/java/nl/beeldengeluid/util/FileLoader.java
+++ src/main/java/nl/beeldengeluid/util/FileLoader.java
@@ -1,19 +1,23 @@
 package nl.beeldengeluid.util;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
 
 public class FileLoader {
     public List<String> loadLines(Path csvFilePath) {
-        return null;
+        try {
+            return Files.readAllLines(csvFilePath, StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            throw new FileLoadingException("Could not read file: " + csvFilePath, e);
+        }
     }
 
     public String loadText(Path txtFilePath) {
         try {
-
-            return Files.readString(txtFilePath);
+            return Files.readString(txtFilePath, StandardCharsets.UTF_8);
         } catch (IOException e) {
             throw new FileLoadingException("Could not read file: " + txtFilePath, e);
         }
--- src/test/java/util/FileLoaderTest.java
+++ src/test/java/util/FileLoaderTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.nio.file.Path;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -27,8 +28,21 @@ public class FileLoaderTest {
     }
 
     @Test
-    void throwsErrorIfInvalidFilePath() {
+    void throwsErrorIfInvalidTxtFilePath() {
         Path path = Path.of("src/test/resources/incorrect-file-path.txt");
         assertThrows(FileLoadingException.class, () -> loader.loadText(path));
+    }
+
+    @Test
+    void readsCsvContentSuccessfully() {
+        Path path = Path.of("src/test/resources/example.csv");
+        List<String> lines = loader.loadLines(path);
+        assertFalse(lines.isEmpty());
+    }
+
+    @Test
+    void throwsErrorIfInvalidFileCsvPath() {
+        Path path = Path.of("src/test/resources/incorrect-file-path.csv");
+        assertThrows(FileLoadingException.class, () -> loader.loadLines(path));
     }
 }
